39612c1208f2b5bce29224b07cc66aba1e0cc039
kritgarg <kritg0160@gmail.com>
2026-05-30 14:53:54 +0530

    feat: integrate Electron with React for desktop application

---
+++ electron/main.cjs
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  })
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+function handle(){
+  alert(window.electronAPI.test())
+}
+
 const App = () => {
   return (
     <div>
-      
+      <h1>hello</h1>
+      <button onClick={handle}> test krit</button>
     </div>
   )
 }
